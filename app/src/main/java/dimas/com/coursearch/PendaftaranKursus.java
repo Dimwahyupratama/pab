a93371c34d2d5ba67208a38ccce1a216d8c500aa
unknown <18523223@students.uii.ac.id>
2020-12-20 11:02:56 +0700

    first commit

---
+++ app/src/main/java/dimas/com/coursearch/PendaftaranKursus.java
@@ -0,0 +1,4 @@
+package dimas.com.coursearch;
+
+public class PendaftaranKursus {
+}
---
+++ app/src/main/java/dimas/com/coursearch/pendaftaranActivity.java
@@ -0,0 +1,14 @@
+package dimas.com.coursearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pendaftaranActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendaftaran);
+    }
+}
